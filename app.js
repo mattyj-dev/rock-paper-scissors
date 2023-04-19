d6485aa4ca90e1ff04819af6ffe67bd14eee9793
mattyj-dev <mjuleson@gmail.com>
2023-04-19 06:50:21 -0700

    create comments

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+//write a function that randomly returns a)rock - b)paper - c)scissors
 
+//create R-P-S drop-down for the human side input
+
+//create function that pulls random computer selection and matches it against human selection
+
+//declare result of winner e.g. "You lose! Paper beats Rock"
