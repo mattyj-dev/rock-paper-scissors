e8e4f88546ed1a18c30c8ce0507b078a48f1c639
mattyj-dev <mjuleson@gmail.com>
2023-04-22 14:38:48 -0700

    update

--- app.js
+++ app.js
@@ -3,18 +3,42 @@ const choices = ["rock", "paper", "scissors"]
 
 function game() {
     //play the game
+    playRound();
+
     //play five rounds
     //console based
 }
 
 function playRound() {
-    const playerSelection =
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
 }
 
 function playerChoice() {
     //get input from player
+    let input = prompt('Type: Rock, Paper, or Scissors');
+    while(input == null) {
+        input = prompt('Type: Rock, Paper, or Scissors');
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while(check == false) {
+        prompt('Type: Rock, Paper, or Scissors -- Capitalization is insignificant however spelling is imperative!');
+    }
+    //console.log(input);
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)]
 }
+
+
+function validateInput(choice) {
+    if(choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+game();
